7696c1c79a51d43baf0a081aec6bd79eb4ce80a5
Sanjaygehlot1 <sanjaygehlot1695@gmail.com>
2025-09-17 19:55:33 +0530

    minor fix for deployment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import DBConnect from "./DataBase/DBConnection.js";
 import dotenv from 'dotenv'
 import { app } from "./app.js";
+import serverless from 'serverless-http'; 
+
 dotenv.config({
     path: ".env"
 })
 
-DBConnect()
-.then(()=>{
-    app.listen(process.env.PORT || 3000 ,()=>{
-        console.log("app listening at port:", process.env.PORT)
-    }) 
-})
-.catch((err)=>{
+DBConnect().catch((err)=>{
     console.log("Error Connecting to Database ", err)
 })
+
+export default serverless(app);
